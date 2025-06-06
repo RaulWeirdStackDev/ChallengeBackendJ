0869cb0800ec7da34e9b3aa5454ee662f5f1f439
Raúl <raul.rodriguez.c.2012@gmail.com>
2025-06-05 23:32:13 -0400

    index y trabajo en rutas

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express, {Application} from 'express';
+import dotenv from 'dotenv';
+import { productsRouter } from './routes/productsRoutes';
+import { authRouter } from './routes/authRoutes';
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+const app: Application = express();
+app.use(express.json());
+
+app.use("/api", productsRouter );
+app.use("/api", authRouter );
+
+app.listen(PORT, () => {
+    console.log(`El servidor está corriendo en el puerto ${PORT}`);
+});
---
+++ src/routes/productsRoutes.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+
+export const productsRouter = Router();
+
+productsRouter.get("/products", );
+productsRouter.get("/products/:id", );
+productsRouter.post("/products", );
+productsRouter.put("/products/:id", );
+productsRouter.delete("/products/:id", );
