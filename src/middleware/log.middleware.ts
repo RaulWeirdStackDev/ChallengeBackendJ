60fd0ee135f3a6c9016a5e68e32596015fefd09f
Raúl <raul.rodriguez.c.2012@gmail.com>
2025-06-12 23:38:33 -0400

    types en log.middleware

--- src/middleware/log.middleware.ts
+++ src/middleware/log.middleware.ts
@@ -1,9 +1,11 @@
-export const log = (req, _, next) => {
-    console.log({
-        method: req.method,
-        body: req.body,
-        params: req.params,
-        query: req.query
-    })
-    next()
-}
+import { Request, Response, NextFunction } from 'express';
+
+export const log = (req: Request, _: Response, next: NextFunction) => {
+  console.log({
+    method: req.method,
+    body: req.body,
+    params: req.params,
+    query: req.query,
+  });
+  next();
+};
